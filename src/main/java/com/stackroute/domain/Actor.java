00e6a6f3c720d8236785c35805e370185016d697
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-07-19 09:33:21 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -4,14 +4,15 @@ public class Actor {
     private String name;
     private String gender;
     private int age;
+    //method to set actorname
     public void setName(String name) {
         this.name = name;
     }
-
+    //method to set actor gender
     public void setGender(String gender) {
         this.gender = gender;
     }
-
+    //method to set actor age
     public void setAge(int age) {
         this.age = age;
     }
