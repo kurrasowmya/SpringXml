6654d3116fc0db7adc57f80e39b7f7eaa9ac2d0f
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-07-19 09:34:43 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,12 +1,13 @@
 package com.stackroute.domain;
 
 public class Movie {
+    //Declare actor object
     Actor actor;
-
+    //Initialize actor
     public void setActor(Actor actor) {
         this.actor = actor;
     }
-
+    //print actor details
     public void movieDetails()
     {
        actor.printActorDetails();
