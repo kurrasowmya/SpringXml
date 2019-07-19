0212165459f4e8ca1bd21ae3f3b3062693463752
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-07-19 09:35:48 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,7 +9,9 @@ public class Main {
     public static void main(String[] args)
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        //get bean containing id movie
         Movie ram=context.getBean("movie",Movie.class);
+        //call the method to display actor details
         ram.movieDetails();
 
     }
