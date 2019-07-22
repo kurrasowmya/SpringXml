cd94810f938a0d4376cbffb910c246c87ec5099d
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-07-22 09:13:46 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,15 +1,18 @@
 package com.stackroute.domain;
 
 public class Actor {
+    //Declare variables for Actor class
     private String name;
     private String gender;
     private int age;
+    //parameterized constructor to set the variables
     public Actor(String name,String gender,int age)
     {
         this.name=name;
         this.gender=gender;
         this.age=age;
     }
+    //method to print the actor details
     public void printActorDetails()
     {
         System.out.println("Name:"+name);
