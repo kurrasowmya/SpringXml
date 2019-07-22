3b1597f615a1ad29287282d081ba9dcc0bbe9341
kurrasowmya <51942892+kurrasowmya@users.noreply.github.com>
2019-07-22 09:15:55 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,13 +1,14 @@
 package com.stackroute.domain;
 
 public class Movie {
+    //decalre a object for actor class
     Actor actor;
+    //constructor for actor class to initialize object
     public Movie(Actor actor)
     {
         this.actor=actor;
     }
-
-
+    //method to call the method of actor class
     public void movieDetails()
     {
        actor.printActorDetails();
